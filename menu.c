5668becd25e8d401d0275230c670f71197a97287
Ashish B S <ashishbs@jnnce.ac.in>
2026-06-17 11:01:45 +0000

    Half

---
+++ menu.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    char op;
+
+    printf("<----------Welcome to my Dhaba---------->");
+    printf("Select what you want-> \n");
+    printf("1. Starter\n2. Main Course\n3. Dessert\n4. Exit\n");
+    scanf("%c",&op);
+    switch(op)
+    {
+        case'1':
+        printf("You have choosed starter\n");
+        printf("1. Chicken 65\n2. Dragon Chicken\n3. Paneer Pakoda\n");
+        break;
+        case'2':
+        printf("You have choosed main course\n");
+        printf("1. \n2.\n3. \n");
+        break;
+        case'3':
+        printf("You have choosed dessert\n");
+        printf("1. \n2. \n3. \n");
+        break;
+        case'4':
+        printf("Thank you visit again\n");
+        break;
+        default:
+        printf("Sorry try next time\n");
+        break;
+    }
+}
